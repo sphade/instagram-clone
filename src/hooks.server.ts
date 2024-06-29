209fce59335701a21ecfaae79ed9c91162988bcd
lawal <sphade010@gmail.com>
2024-06-29 18:01:59 +0100

    chore: Update database connection and dependencies

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -10,43 +10,6 @@ const preloadFonts: Handle = async ({ event, resolve }) => {
 	return response;
 };
 
-// export const handleAuth: Handle = async ({ event, resolve }) => {
-
-// 	const sessionId = event.cookies.get(lucia.sessionCookieName);
-// 	if (!sessionId) {
-// 		event.locals.user = null;
-// 		event.locals.session = null;
-// 		return resolve(event);
-// 	}
-
-// 	const { session, user } = await lucia.validateSession(sessionId);
-// 	if (session && session.fresh) {
-// 		const sessionCookie = lucia.createSessionCookie(session.id);
-
-// 		event.cookies.set(sessionCookie.name, sessionCookie.value, {
-// 			path: '.',
-// 			...sessionCookie.attributes
-// 		});
-// 	}
-// 	if (!session) {
-// 		const sessionCookie = lucia.createBlankSessionCookie();
-// 		event.cookies.set(sessionCookie.name, sessionCookie.value, {
-// 			path: '.',
-// 			...sessionCookie.attributes
-// 		});
-// 	}
-// 	event.locals.user = user;
-// 	event.locals.session = session;
-// 	if (
-// 		event.url.pathname.startsWith('/') &&
-// 		!event.locals.user &&
-// 		!['/login', '/register'].includes(event.url.pathname)
-// 	) {
-// 		console.log('redirecting to login');
-// 		redirect(303, '/login');
-// 	}
-// 	return resolve(event);
-// };
 export const handleAuth: Handle = async ({ event, resolve }) => {
 	const sessionId = event.cookies.get(lucia.sessionCookieName) || '';
 
